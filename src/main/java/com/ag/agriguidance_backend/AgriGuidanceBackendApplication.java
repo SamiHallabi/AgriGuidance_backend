2c96b71dfe81bbb0290e74917ed52a6a53e1ca03
samih <samihallabi0@gmail.com>
2025-08-01 17:50:45 +0100

    first commit

---
+++ src/main/java/com/ag/agriguidance_backend/AgriGuidanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ag.agriguidance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriGuidanceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriGuidanceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ag/agriguidance_backend/AgriGuidanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.agriguidance_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriGuidanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
